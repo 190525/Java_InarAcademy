e6808c6c0a86309c35c805d0a90250b9378faf72
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-22 20:07:57 +0300

    Calculation Of Exponent Operation

---
+++ src/chapters/chapter_02/Exercise_06.java
@@ -0,0 +1,9 @@
+package chapters.chapter_02;
+//CalculationOfExponentOperation
+public class Exercise_06 {
+	public static void main(String[] args) {
+	double m= 5;
+	double r = 8.6;
+	System.out.println(m * Math.pow(r ,2));
+	}
+}
