495ec12c5d2076c8f8e5333dfb2d007ad34e87c3
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-22 20:01:47 +0300

    Result Of Calculations

---
+++ src/chapters/chapter_02/Exercise_03.java
@@ -0,0 +1,12 @@
+package chapters.chapter_02;
+// ResultOfCalculations
+public class Exercise_03 {
+	public static void main(String[] args) {
+	System.out.println(2 * (5 / 2 + 5 / 2));
+	System.out.println(2 * 5 / 2 + 2 * 5 / 2);
+	System.out.println(2 * (5 / 2));
+	System.out.println(2 * 5 / 2);
+}
+
+}
+
