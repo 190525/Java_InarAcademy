f32adeddbfc0d483deb65f1fbd033353149875b1
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-25 23:17:04 +0300

    /Algebra: solve 2 * 2 linear equations

---
+++ src/chapters/chapter_03/Exercise_03.java
@@ -0,0 +1,31 @@
+package chapters.chapter_03;
+
+//Algebra: solve 2 * 2 linear equations
+import java.util.*;
+
+public class Exercise_03 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a, b ,c ,d ,e ,f : ");
+
+		double a = input.nextDouble();
+		double b = input.nextDouble();
+		double c = input.nextDouble();
+		double d = input.nextDouble();
+		double e = input.nextDouble();
+		double f = input.nextDouble();
+		
+		if ((a * d - b * c) != 0) {
+			double x= (e * d - b * c) / (a * d - b * c);
+			double y= (a * f - b * f) / (a * d - b * c);
+			System.out.println("x is " + x+" and y is " + y);
+			}
+		
+			else {
+				System.out.println("The equation has no solution");
+			
+		}
+	}
+
+}
+
