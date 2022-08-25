a878321f3da01bca79ac1cb60b83fb044d830433
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-25 23:39:52 +0300

    Random point

---
+++ src/chapters/chapter_03/Exercise_16.java
@@ -0,0 +1,13 @@
+package chapters.chapter_03;
+// Random point
+public class Exercise_16 {
+	 public static void main(String[] args) {
+		  double x = (int)((Math.random() * 50-25) * 100) / 100.;
+		  double y = (int)((Math.random() * 75-50) * 100) / 100.;
+		   
+		  System.out.print("("+x + "," + y + ")");
+		 }
+		 
+		}
+
+
