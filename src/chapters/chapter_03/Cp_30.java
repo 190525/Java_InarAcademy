7d2c83b3833721c1df4bd82630e74f7ea6167c65
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-27 01:25:39 +0300

    rewriting switch case

---
+++ src/chapters/chapter_03/Cp_30.java
@@ -0,0 +1,16 @@
+package chapters.chapter_03;
+
+// rewriting switch case
+public class Cp_30 {
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 3;
+		if (x + 3 == 6) {
+			y = 1;
+		} else
+			y += 1;
+		System.out.println(y);
+
+	}
+}
+
