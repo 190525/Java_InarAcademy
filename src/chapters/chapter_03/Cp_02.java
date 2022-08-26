31218815e6bdf441e2cae8aa0096bbf7e1078fee
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-27 01:12:26 +0300

    Boolean Expression

---
+++ src/chapters/chapter_03/Cp_02.java
@@ -0,0 +1,15 @@
+package chapters.chapter_03;
+//Boolean Expression
+public class Cp_02 {
+	public static void main(String[] args) {
+		int x=1;
+		System.out.println(x>0);
+		System.out.println(x<0);
+		System.out.println(x!=0);
+		System.out.println(x>=0);
+		System.out.println(x<=0);
+	}
+
+
+}
+
