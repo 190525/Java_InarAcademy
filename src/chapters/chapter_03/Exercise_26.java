4fce598213939f37f1f87808eb7f045642479f44
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-26 23:08:56 +0300

    Use the &&, || and ^ operators

---
+++ src/chapters/chapter_03/Exercise_26.java
@@ -0,0 +1,18 @@
+package chapters.chapter_03;
+//Use the &&, || and ^ operators
+import java.util.*;
+public class Exercise_26 {
+	public static void main(String[] args) {
+		 Scanner input = new Scanner(System.in);
+		 
+		  System.out.print("Enter an integer:");
+		  int number = input.nextInt();
+		  
+		  if(number %5 ==0 && number %4 ==0) {
+			  System.out.println();
+		  }
+
+	}
+
+}
+
