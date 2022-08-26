c5a09f5d191ed3f9383e09cdd51889f417e4c947
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-26 23:17:27 +0300

    Current time

---
+++ src/chapters/chapter_03/Exercise_30.java
@@ -0,0 +1,36 @@
+package chapters.chapter_03;
+//Current time
+import java.util.Scanner;
+
+public class Exercise_30 {
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the time zone offset to GMT:");
+		int offset = input.nextInt();
+		// calculate offset in seconds
+		offset = offset * 60 * 60;
+
+		// Obtain the total milliseconds since midnight, Jan 1, 1970
+		double totalMilliseconds = System.currentTimeMillis();
+
+		double totalSeconds = (totalMilliseconds / 1000) + offset;
+		int seconds = (int) totalSeconds % 60;
+
+		double totalMinutes = totalSeconds / 60;
+		int minutes = (int) totalMinutes % 60;
+
+		double totalHours = totalMinutes / 60;
+		int hours = (int) (totalHours )% 24;
+
+		if (hours < 12) {
+			System.out.println("Current time is " + hours + ":" + minutes + ":" + seconds + " a.m");
+
+		} else {
+			System.out.println("Current time is " + (hours - 12) + ":" + minutes + ":" + seconds + " p.m");
+
+		}
+
+	}
+}
+
