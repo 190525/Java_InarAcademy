0878fea695d1f442efd0b281141f399610edf5a0
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-27 01:23:38 +0300

    boolean expression(weight- height)

---
+++ src/chapters/chapter_03/Cp_27.java
@@ -0,0 +1,23 @@
+package chapters.chapter_03;
+// boolean expression(weight- height)
+
+import java.util.Scanner;
+
+public class Cp_27 {
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		System.out.println("Enter weight in pouds :");
+		int weight=input.nextInt();
+		System.out.println("Enter weight height in inches :");
+		int height=input.nextInt();
+		if(weight>50 && height>60) {
+			System.out.println(weight>50 && height>60);
+		}
+		else
+			System.out.println(weight>50 && height>60);
+
+}
+}
+
+
+
