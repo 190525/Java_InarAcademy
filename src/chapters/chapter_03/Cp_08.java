2ac9a9327fdc88c8f0e652a39d9274e8b1dc781c
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-27 01:13:35 +0300

    output if

---
+++ src/chapters/chapter_03/Cp_08.java
@@ -0,0 +1,30 @@
+package chapters.chapter_03;
+// output if
+public class Cp_08 {
+	public static void main(String[] args) {
+		int x;
+		int y;
+		x = 3;
+		y = 4;
+		int z;
+		if (x > 2) {
+			if (y > 2) {
+				z = x + y;
+				System.out.println("z is " + z);
+			}
+		} else
+			System.out.println("x is " + x);
+		x=2;
+		y=2;
+		if (x > 2) {
+			if (y > 2) {
+				z = x + y;
+				System.out.println("z is " + z);
+			}
+		} else
+			System.out.println("x is " + x);
+
+	}
+
+}
+
