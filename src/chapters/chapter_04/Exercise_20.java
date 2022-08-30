62c61e933d9f086ff3fd777823c13c7f570ca47b
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-30 23:31:53 +0300

    Process a string

---
+++ src/chapters/chapter_04/Exercise_20.java
@@ -0,0 +1,17 @@
+package chapters.chapter_04;
+// Process a string
+import java.util.*;
+
+public class Exercise_20 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		 
+		  System.out.println("Enter string:");
+		  String sentence = input.nextLine();
+		  System.out.println("The string length is " + sentence.length());
+		  System.out.println("The first character is " + sentence.charAt(0));
+
+	}
+
+}
+
