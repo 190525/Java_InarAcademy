a09d2a154ec18feee5671ed8b7f94644d09fce67
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-31 12:46:09 +0300

    Edit Random point

--- src/chapters/chapter_03/Exercise_16.java
+++ src/chapters/chapter_03/Exercise_16.java
@@ -2,12 +2,14 @@ package chapters.chapter_03;
 // Random point
 public class Exercise_16 {
 	 public static void main(String[] args) {
-		  double x = (int)((Math.random() * 50-25) * 100) / 100.;
-		  double y = (int)((Math.random() * 75-50) * 100) / 100.;
-		   
-		  System.out.print("("+x + "," + y + ")");
-		 }
-		 
-		}
+		
+		double x1 = Math.random() * 101 - 50;
+		double y1 = Math.random() * 201 - 100;
+		// editing form of x and y
+		double x = (int) (x1 * 100) / 100.;
+		double y = (int) (y1 * 100) / 100.;
 
+		System.out.print("(" + x + "," + y + ")");
+	}
 
+}
