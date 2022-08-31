d203b21a23b6009c31db925104aafe6a3539735c
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-31 23:17:32 +0300

    Show the output

---
+++ src/chapters/chapter_04/Cp_19.java
@@ -0,0 +1,14 @@
+package chapters.chapter_04;
+// Show the output
+public class Cp_19 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1 + "Welcome " + 1 + 1);
+		System.out.println(1 + "Welcome " + (1 + 1));
+		System.out.println(1 + "Welcome " + ('\u0001' + 1));
+		System.out.println(1 + "Welcome " + 'a' + 1);
+	}
+
+}
+
