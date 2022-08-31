021ffe5d103b5c8ec7465459b91cf3266c922559
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-31 23:15:01 +0300

    Compare character

---
+++ src/chapters/chapter_04/Cp_15.java
@@ -0,0 +1,18 @@
+package chapters.chapter_04;
+// Compare character
+public class Cp_15 {
+
+	public static void main(String[] args) {
+		System.out.println('a' < 'b');
+		System.out.println('a' <= 'A');
+		System.out.println('a' > 'b');
+		System.out.println('a' >= 'A');
+		System.out.println('a' == 'a');
+		System.out.println('a' != 'b');
+		
+		
+	
+	}
+
+}
+
