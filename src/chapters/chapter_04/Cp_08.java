76399aff4ca36cd3933c6e948db1dcd3f58895c6
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-31 23:10:41 +0300

    ASCII Codes to decimal codes and decimal codes to ASCII Codes

---
+++ src/chapters/chapter_04/Cp_08.java
@@ -0,0 +1,42 @@
+package chapters.chapter_04;
+
+//ASCII Codes to decimal codes and decimal codes to ASCII Codes
+public class Cp_08 {
+
+	public static void main(String[] args) {
+		int x = '1';
+		int y = 'A';
+		int z = 'B';
+		int k = 'a';
+		int q = 'b';
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(k);
+		System.out.println(q);
+
+		char ch = 40;
+		char ch1 = 59;
+		char ch2 = 79;
+		char ch3 = 85;
+		char ch4 = 90;
+		System.out.println(ch);
+		System.out.println(ch1);
+		System.out.println(ch2);
+		System.out.println(ch3);
+		System.out.println(ch4);
+		int numb = '4' + '0';
+		int numb1 = '5' + 'A';
+		int numb2 = '7' + '1';
+		int numb3 = '7' + '2';
+		int numb4 = '7' + 'A';
+		System.out.println(numb);
+		System.out.println(numb1);
+		System.out.println(numb1);
+		System.out.println(numb3);
+		System.out.println(numb4);
+
+	}
+
+}
+
