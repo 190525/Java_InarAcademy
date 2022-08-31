a25bcf9fb1d03f1c0cf16c42166b613ea3dd7be6
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-08-31 23:16:40 +0300

    Show the output

---
+++ src/chapters/chapter_04/Cp_18.java
@@ -0,0 +1,14 @@
+package chapters.chapter_04;
+// Show the output
+public class Cp_18 {
+
+	public static void main(String[] args) {
+		System.out.println("1" + 1);
+		System.out.println('1' + 1);
+		System.out.println("1" + 1 + 1);
+		System.out.println("1" + (1 + 1));
+		System.out.println('1' + 1 + 1);
+	}
+
+}
+
