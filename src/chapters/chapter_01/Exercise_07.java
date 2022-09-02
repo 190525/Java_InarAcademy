9b6fa97c2f2119ed5af721484fc8e37b151faa45
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-09-02 17:02:26 +0300

    Approximate pi calculation

---
+++ src/chapters/chapter_01/Exercise_07.java
@@ -0,0 +1,13 @@
+package unite1;
+// Approximate pi calculation
+public class alistirma6 {
+	public static void main(String[] args) {
+		double Pi1 = 4 * (1.0 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11);
+		double Pi2 = 4 * (1.0 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11 + 1.0 / 13);
+
+		System.out.println(Pi1);
+		System.out.println(Pi2);
+		
+	}
+
+}
