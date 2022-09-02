731085f2cbe1bcee9d6bde58f2e06ea7b0dec7af
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-09-02 17:12:05 +0300

    Average speed in miles

---
+++ src/chapters/chapter_01/Exercise_10.java
@@ -0,0 +1,18 @@
+package chapter_01
+
+public class Exercise_10 {
+
+    public static void main(String[] strings) {
+
+        double kilometers = 14.0;
+        double miles = kilometers / 1.6;
+
+        double rate = (45.5 * 60.0 + 30.0) / (60.0 * 60.0);
+        double milesPerHour = miles / rate;
+
+        System.out.println(milesPerHour);
+
+
+    }
+
+}
