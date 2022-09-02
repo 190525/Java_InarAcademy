3cf97757520eaac26e9dc05805d594e0da71c8ae
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-09-02 16:45:46 +0300

    Display three messages

---
+++ src/chapters/chapter_01/Exercise_01.java
@@ -0,0 +1,12 @@
+package unite1;
+
+public class Exercise_01 {
+	public static void main(String[] args) {
+	
+		 System.out.println("Welcome to Java!");
+		 System.out.println("Welcome to Computer Science!");
+		 System.out.println("Welcome to Java!");
+		 
+		 }
+
+}
