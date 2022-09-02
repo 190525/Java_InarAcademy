fb9d75d70369a4e1bec0ea068b374762499ab501
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-09-02 17:03:59 +0300

    Area and perimeter of a circle

---
+++ src/chapters/chapter_01/Exercise_08.java
@@ -0,0 +1,17 @@
+package unite1;
+//Area and perimeter of a circle
+public class Exeercise_08 {
+	public static void main(String[] args) {
+
+        double radius = 5.5;
+        double pi = 3.14159;
+        double perimeter = 2 * radius * pi;
+        double area = radius * radius * pi;
+
+        System.out.println("Area of a circle with radius 5.5 is: " + area);
+        System.out.println("Perimeter of a circle with radius 5.5 is: " + perimeter);
+        
+
+	}
+
+}
