aece72135384c2762c1f804678f708afcecff4d7
İsmail aslan <111380637+190525@users.noreply.github.com>
2022-09-02 17:23:43 +0300

    Approximate pi calculation

--- src/chapters/chapter_01/Exercise_07.java
+++ src/chapters/chapter_01/Exercise_07.java
@@ -1,6 +1,6 @@
 package chapter_01;
 // Approximate pi calculation
-public class Exercise_06 {
+public class Exercise_07 {
 	public static void main(String[] args) {
 		double Pi1 = 4 * (1.0 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11);
 		double Pi2 = 4 * (1.0 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11 + 1.0 / 13);
