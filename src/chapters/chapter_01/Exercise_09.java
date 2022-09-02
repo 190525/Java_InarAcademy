4ca75bdbbda229e3342a136568b2c644c9414898
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-09-02 17:06:57 +0300

    Area and perimeter of a rectangle

---
+++ src/chapters/chapter_01/Exercise_09.java
@@ -0,0 +1,13 @@
+package chapter_01
+public class Exercise_09 {
+
+    public static void main(String[] strings) {
+
+        double width = 4.5;
+        double height = 7.9;
+
+        double area = width * height;
+
+        System.out.println("Rectangle area is " + area);
+    }
+}
