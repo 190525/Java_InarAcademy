6ffccf8889981716ace5406262acaad9a60a6d79
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-09-02 16:58:00 +0300

    compute expression

---
+++ src/chapters/chapter_01/Exercise_05.java
@@ -0,0 +1,9 @@
+package unite1;
+
+public class Exercise_05 {
+	public static void main(String[] args) {
+		 System.out.print("(10.5 + 2 * 3) / (45 – 3.5) = ");
+		 System.out.println((10.5 + 2 * 3) /(45 - 3.5));
+		 }
+
+}
