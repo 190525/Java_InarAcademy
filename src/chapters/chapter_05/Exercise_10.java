4d2521b37825c5efffcf1173fd69c9c54dca40c6
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-09-07 20:33:19 +0300

    Find numbers divisible by 5 and 6

---
+++ src/chapters/chapter_05/Exercise_10.java
@@ -0,0 +1,22 @@
+package chapters.chapter_05;
+//Find numbers divisible by 5 and 6
+public class Exercise_10 {
+
+	public static void main(String[] args) {
+		int countPerLine=0;
+		for (int i = 100; i <= 1000; i++) {
+			if (i % 5 == 0 && i % 6 == 0) {
+				System.out.printf("%4d",i);
+				countPerLine++;
+				if(countPerLine%10==0) {
+					System.out.println();
+				}
+
+			}
+		}
+
+	}
+
+}
+:
+
