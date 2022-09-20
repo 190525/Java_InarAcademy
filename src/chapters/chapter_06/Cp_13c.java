fec367951332ea608bbead1754b3f3a09fa379fa
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-09-20 23:48:23 +0300

    to use different method at 13.Checkpoint

---
+++ src/chapters/chapter_06/Cp_13c.java
@@ -0,0 +1,20 @@
+public class Cp_13c {
+    public static void main(String[] args) {
+        // Initialize times
+        int times=3;
+        System.out.println("Before the call, variable times is "+times);
+        // Invoke nPrintln and display times
+        nPrintln("Welcome to Java!", times);
+        System.out.println("After the call, variable times is "+times);
+    }
+
+    public static void nPrintln(String s, int n) {
+        while (n>0){
+            System.out.println("n = " + n);
+            System.out.println(s);
+            n--;
+        }
+
+    }
+}
+
