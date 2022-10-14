3491cfab8f6228006394f2b1a79bcfc180ac747a
İsmail aslan <111380637+190525@users.noreply.github.com>
2022-10-14 07:53:13 +0300

    Random number chooser

--- src/chapters/chapter_07/Exercise_13.java
+++ src/chapters/chapter_07/Exercise_13.java
@@ -7,6 +7,11 @@ public class Exercise_13 {
     }
     public static int getRandom(int... numbers){
        int  number=(int)(Math.random()*54)+1;
+        for (int num : numbers) {
+            if (number == num) {
+                return getRandom(numbers);
+            }
+        }
         return number;
     }
 }
