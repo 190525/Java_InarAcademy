76f2460dfb9f1c6b0eaaec66410a53718a835b2a
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-11-03 12:47:48 +0300

    Use the GregorianCalendar class

---
+++ src/chapters/chapter_09/Exercise_05.java
@@ -0,0 +1,21 @@
+import java.util.GregorianCalendar;
+
+// Use the GregorianCalendar class
+public class Exercise_05 {
+    public static void main(String[] args) {
+        GregorianCalendar date=new GregorianCalendar();
+
+        System.out.println(date.get(GregorianCalendar.YEAR));
+        System.out.println(date.get(GregorianCalendar.MONTH));
+        System.out.println(date.get(GregorianCalendar.DAY_OF_MONTH));
+
+        date.setTimeInMillis(1234567898765L);
+        System.out.println(date.get(GregorianCalendar.YEAR));
+        System.out.println(date.get(GregorianCalendar.MONTH));
+        System.out.println(date.get(GregorianCalendar.DAY_OF_MONTH));
+
+
+    }
+}
+
+
