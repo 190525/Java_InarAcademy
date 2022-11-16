6a8553c328696137c425d125f12345373b015642
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-11-16 22:30:09 +0300

    My Stack

---
+++ src/chapters/chapter_11/Listing_10.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+//Listing_10
+public class MyStack {
+    private ArrayList<Object> list=new ArrayList<>();
+
+    public boolean isEmpty(){
+        return list.isEmpty();
+    }
+    public int getSize(){
+        return list.size();
+    }
+    public Object peek() {
+        return list.get(getSize() - 1);
+    }
+    public Object pop(){
+        Object o=list.get(getSize() - 1);
+        list.remove(o);
+        return o;
+    }
+    public void push(Object o){
+        list.add(o);
+    }
+    @Override
+    public String toString(){
+        return "stack: " + list.toString();
+    }
+}
+
