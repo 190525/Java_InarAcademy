dd11911b655994c76f292584290db8c149eaf611
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-11-16 22:23:43 +0300

    Casting Demo

---
+++ src/chapters/chapter_11/Listing_07.java
@@ -0,0 +1,26 @@
+
+// Listing_07
+public class CastingDemo {
+    public static void main(String[] args) {
+        Object object1 = new CircleFromSimpleGeometricObject(1);
+        Object object2 = new RectangleFromSimpleGeometricObject(1, 1);
+        display(object1);
+        display(object2);
+
+
+
+    }
+
+    private static void display(Object object) {
+        if(object instanceof CircleFromSimpleGeometricObject){
+            System.out.println("The circle area is " + ((CircleFromSimpleGeometricObject)object).getArea());
+            System.out.println("The circle diameter is " + ((CircleFromSimpleGeometricObject)object).getDiameter());
+
+        }
+        else if(object instanceof RectangleFromSimpleGeometricObject){
+            System.out.println("The rectangle area is " + ((RectangleFromSimpleGeometricObject)object).getArea());
+
+        }
+    }
+}
+
