6d2628abf0bdf23347cd3fcc6b59ffd1146840e7
İsmail aslan <111380637+190525@users.noreply.github.com>
2022-11-19 14:31:44 +0300

    Implement MyStack using inheritance

---
+++ src/chapters/chapter_11/Exercise_10/MyStack.java
@@ -0,0 +1,26 @@
+package chapter_11.Exercise_10;
+
+import java.util.ArrayList;
+public class MyStack  extends ArrayList<Object> {
+
+    public Object peek() {
+        return get(size() - 1);
+    }
+
+    public Object pop() {
+        Object o = get(size() - 1);
+        remove(size() - 1);
+        return o;
+    }
+
+    public void push(Object o) {
+        add(o);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Stack:\n" + super.toString();
+    }
+}
+
