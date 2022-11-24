3c315263299eeebc521199d4d3e6a9e0340c6e20
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-11-24 22:12:05 +0300

    IllegalArgumentException

---
+++ src/chapters/chapter_12/Exercise_04/Exercise_04.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exercise_04 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+            System.out.println("Enter annual interest rate (for example, 8.25): ");
+            double annualInterestRate = input.nextDouble();
+            System.out.print("Enter number of years as an integer: ");
+            int numberOfYears = input.nextInt();
+            System.out.print("Enter loan amount (for example, 120000.95): ");
+            double loanAmount = input.nextDouble();
+        try {
+            Loan loan = new Loan(annualInterestRate, numberOfYears, loanAmount);
+            System.out.println("The loan was created " + loan.getLoanDate());
+            System.out.println("The monthly payment: " + loan.getMonthlyPayment() + "\nThe total payment: " + loan.getTotalPayment());
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+}
