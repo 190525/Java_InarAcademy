d6c52a96e668b0c212d7f06ec6fcdd703e92c35b
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-11-25 11:03:44 +0300

    BinaryFormatException

---
+++ src/chapters/chapter_12/Exercise_09/BinaryFormatException.java
@@ -0,0 +1,8 @@
+package chapter_12.Exercise_09;
+
+public class BinaryFormatException extends Exception {
+    public BinaryFormatException(){
+        System.out.println("Wrong input!!!Entered number must be binary number.");
+    }
+}
+
