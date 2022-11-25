7b6277bb8a951fbb4f2d0ee65fa8a9ab4a97db59
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-11-25 11:02:00 +0300

    HexFormatException

---
+++ src/chapters/chapter_12/Exercise_08/HexFormatException.java
@@ -0,0 +1,8 @@
+package chapter_12.Exercise_08;
+
+public class HexFormatException extends Exception {
+    public HexFormatException(){
+        System.out.println("Wrong input!!!Entered number must be hex number");
+    }
+}
+
