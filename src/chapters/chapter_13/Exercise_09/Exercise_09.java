ea7d66a4b17c4580eea0c1becac914a025311e72
İsmail ASLAN <ismailaslan190525@gmail.com>
2022-12-03 02:52:23 +0300

    Enable Circle comparable

---
+++ src/chapters/chapter_13/Exercise_09/Exercise_09.java
@@ -0,0 +1,14 @@
+package chapter_13.Exercise_09;
+
+public class Exercise_09 {
+    public static void main(String[] args) {
+        Circle c1=new Circle(5);
+        Circle c2=new Circle(5);
+        Circle c3=new Circle(7);
+        System.out.println("Is c1 equals c2 ? "+c1.equals(c2));
+        System.out.println("Is c1 equals c3 ? "+c1.equals(c3));
+
+
+    }
+}
+
